322edd09c5c0f50e2e7015fdc967f757b4f388f1
Romulo Nogueira <romulonogueira929@gmail.com>
2024-05-20 23:28:51 -0300

    atualização2

---
+++ Aula-27-05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_27_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
